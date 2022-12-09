1ea7df6c2f9e97da7ff1679351577163a939463d
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-12-10 02:31:16 +0400

    xterm

--- client/app.js
+++ client/app.js
@@ -1,16 +1,29 @@
 //const socket = new io("ws://localhost:8080");
 const socket = new io(location.protocol.replace('http', 'ws') + '//' + location.hostname + (location.port ? (':' + location.port) : '') + '/');
-
 var term = new Terminal(
     {
-        FontFace: "Cascadia Mono",
-        cursorStyle: "underline",
+        fontFamily: "Consolas, 'Courier New', monospace",
+        cursorStyle: "block",
         cursorBlink: "true",
-        fontSize: "12",
+        fontSize: "14",
+        cursorWidth: 5
     }
 );
 
-term.open(document.getElementById("terminal"));
+var terminal = document.getElementById('terminal');
+term.open(terminal);
+
+term.prompt = function () {
+    term.write('\r\n');
+};
+
+term.writeln('Requesting a terminal (this might take a while)...');
+term.writeln('Succeeded');
+term.writeln('Welcome to Tron Terminal');
+term.writeln('Connected to a remote terminal');
+
+term.writeln('');
+term.prompt();
 
 socket.on("terminal.incomingData", (data) => {
     term.write(data);

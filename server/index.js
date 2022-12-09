a738003c2966cd5148b34c6645a6cb1d37ead2e3
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-12-09 20:14:41 +0400

    adding express client

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ io.on("connection", (socket) => {
     });
 });
 
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))

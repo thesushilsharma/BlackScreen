f18087918e17c53e804422d58dec03684b8c9029
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-07-14 00:39:47 +0400

    making some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { TerminalEmulator } from "@/components/terminal-emulator";
-import { Terminal, Code, Zap, Globe, Shield, Cpu } from "lucide-react";
 
 export default function Home() {
   return (
   <>
-  <TerminalEmulator className="h-full" />
+  <TerminalEmulator />
   </>
   );
 }

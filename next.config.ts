7fca8e2f56d29c92b170c768dec7e8a37744f867
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-12-15 01:39:43 +0400

    hotfix : patching the next js react

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   eslint: {
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
